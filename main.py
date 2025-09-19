443ac9ff1e38912863106c4a561ea6acc10b6a85
Amine-El-Ameri_biontech <Amine.El-Ameri@biontech.com>
2025-09-19 13:40:18 +0200

    exp: 1340

--- main.py
+++ main.py
@@ -13,19 +13,21 @@ import tensorflow_io as tfio
 
 def aichor_write_tensorboard():
 
-    # TensorBoard remote log path from env var (optional)
-    tb_remote_log_path = os.getenv("AICHOR_TENSORBOARD_PATH")
-    print(f"### AICHOR_LOGS_PATH={tb_remote_log_path}")
+    # # TensorBoard remote log path from env var (optional)
+    # tb_remote_log_path = os.getenv("AICHOR_TENSORBOARD_PATH")
+    # print(f"### AICHOR_LOGS_PATH={tb_remote_log_path}")
     
 
-    remote_writer = tf.summary.create_file_writer(tb_remote_log_path) 
-    # local_writer = tf.summary.create_file_writer(tb_local_log_path) 
+    # remote_writer = tf.summary.create_file_writer(tb_remote_log_path) 
+    # # local_writer = tf.summary.create_file_writer(tb_local_log_path) 
 
-    with remote_writer.as_default():
-        for step,val in enumerate([0.31,0.28,0.24,0.20,0.18], start=5):
-            tf.summary.scalar("demo/loss", val, step=step)
-            remote_writer.flush(); time.sleep(1)
-        print("Appended points to", tb_remote_log_path) 
+    # with remote_writer.as_default():
+    #     for step,val in enumerate([0.31,0.28,0.24,0.20,0.18], start=5):
+    #         tf.summary.scalar("demo/loss", val, step=step)
+    #         remote_writer.flush(); time.sleep(1)
+    #     print("Appended points to", tb_remote_log_path) 
+
+    print("### TEST 123")
     
 
 def main():
