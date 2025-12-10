b166b7f0b51ffb444c45741a477a980f8a0f04b5
Amine-El-Ameri_biontech <Amine.El-Ameri@biontech.com>
2025-12-10 16:19:52 +0100

    exp: clean 1619

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
-FROM python:3.8-slim
+FROM python:3.12-slim
 
-RUN pip install tensorboardX boto3
+RUN pip install tensorboardX boto3 tensorflow tensorflow_io
 
 WORKDIR /app
 COPY ./src ./src
--- main.py
+++ main.py
@@ -1,32 +1,33 @@
-import argparse
+
+
+import os
 import time
+import argparse
 
-from src.operators.jax import jaxop
-from src.operators.ray import rayop
-from src.operators.tf import tfop
 
-from src.utils.tensorboard import dummy_tb_write
+import time
+import tensorflow as tf
+import tensorflow_io as tfio
+
+
+def print_test():
+    # do math multiplications and then print test
+
+    for i in range(10):
+        a = i * 2
+        b = i * 3
+        c = a + b
+        print(f"Test {i}: {c}")
+
+    
+
+def main():
+
+    print_test()
+
+    time.sleep(600)
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='AIchor Smoke test on any operator')
-    parser.add_argument("--operator", type=str, default="tf", choices=["ray", "jax", "tf"],help="operator name")
-    parser.add_argument("--sleep", type=int, default="0", help="sleep time in seconds")
-    parser.add_argument("--tb-write", type=bool, default=False, help="test write to tensorboard")
+    main()
 
-    args = parser.parse_args()
 
-    print(f"using {args.operator} operator")
-
-    if args.operator == "ray":
-        rayop()
-    elif args.operator == "jax":
-        jaxop()
-    elif args.operator == "tf":
-        tfop()
-    
-    if args.tb_write:
-        dummy_tb_write()
-    
-    if args.sleep > 0:
-        print(f"sleeping for {args.sleep}s before exiting")
-        time.sleep(args.sleep)
